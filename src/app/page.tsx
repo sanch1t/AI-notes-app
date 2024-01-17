13a9328d416bd99eb643e1028c4d89771fe0d422
sanch1t <sanchitsingh1503@gmail.com>
2024-01-17 10:09:42 +0530

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-red-600"> Hello</h1>
+    <h1 className="text-red-600"> Hedllo</h1>
   )
   
 }
