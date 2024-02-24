9389f70a0c6eee5649e0a69de975d4f59677d884
sanch1t <sanchitsingh1503@gmail.com>
2024-02-24 12:45:23 +0530

    sASS

--- src/components/TipTapEditor.tsx
+++ src/components/TipTapEditor.tsx
@@ -1,40 +1,65 @@
-'use client'
-import React from 'react'
-import {Editor, EditorContent, useEditor} from '@tiptap/react'
-import {StarterKit} from '@tiptap/starter-kit'
-import { EditorState } from '@tiptap/pm/state'
-import TipTapMenuBar from './TipTapMenuBar'
-import { Button } from './ui/button'
-import { useDebounce } from '@/lib/useDebounce'
-import { useMutation } from '@tanstack/react-query'
-import axios from 'axios'
-import { NoteType } from '@/lib/db/schema'
+"use client";
+import React from "react";
+import { EditorContent, useEditor } from "@tiptap/react";
+import { StarterKit } from "@tiptap/starter-kit";
+import TipTapMenuBar from "./TipTapMenuBar";
+import { Button } from "./ui/button";
+import { useDebounce } from "@/lib/useDebounce";
+import { useMutation } from "@tanstack/react-query";
+import Text from "@tiptap/extension-text";
+import axios from "axios";
+import { NoteType } from "@/lib/db/schema";
+import { useCompletion } from "ai/react";
 
-type Props = {note: NoteType}
+type Props = { note: NoteType };
 
-const TipTapEditor = ({note}: Props) => {
-  
+const TipTapEditor = ({ note }: Props) => {
+  const [editorState, setEditorState] = React.useState(
+    note.editorState || `<h1>${note.name}</h1>`
+  );
+  const { complete, completion } = useCompletion({
+    api: `/api/completion/`,
+  });
+  const saveNote = useMutation({
+    mutationFn: async () => {
+      const response = await axios.post("/api/saveNote", {
+        noteId: note.id,
+        editorState,
+      });
+      return response.data;
+    },
+  });
+  const customText = Text.extend({
+    addKeyboardShortcuts() {
+      return {
+        "Shift-a": () => {
+          // take the last 30 words
+          const prompt = this.editor.getText().split(" ").slice(30).join(" ");
+          complete(prompt);
+          return true;
+        },
+      };
+    },
+  });
 
-  const saveNote = useMutation({mutationFn: async()=>{
-    const response = await axios.post('/api/saveNote',{
-      noteId: note.id,
-      editorState
-    })
-    return response.data
-  }})
-
-
-  const [editorState, setEditorState]= React.useState('')
   const editor = useEditor({
-    autofocus:true,
-    extensions: [StarterKit],
+    autofocus: true,
+    extensions: [StarterKit, customText],
     content: editorState,
-    onUpdate: ({editor}) =>{
-      setEditorState(editor.getHTML())
-    }})
+    onUpdate: ({ editor }) => {
+      setEditorState(editor.getHTML());
+    },
+  });
+  const lastCompletion = React.useRef("");
 
+  React.useEffect(() => {
+    if (!completion || !editor) return;
+    const diff = completion.slice(lastCompletion.current.length);
+    lastCompletion.current = completion;
+    editor.commands.insertContent(diff);
+  }, [completion, editor]);
 
-  const debouncedEditorState = useDebounce(editorState, 1000);
+  const debouncedEditorState = useDebounce(editorState, 500);
   React.useEffect(() => {
     // save to db
     if (debouncedEditorState === "") return;
@@ -47,24 +72,28 @@ const TipTapEditor = ({note}: Props) => {
       },
     });
   }, [debouncedEditorState]);
-
-
-
   return (
     <>
-    <div className='flex flex-row font-mont'>
-      {editor && <TipTapMenuBar editor={editor}/> }
-   
-    <Button 
-      className='font-mont' size='sm' disabled variant={"outline"}>
-        {saveNote.isPending ? "Saving..." : "Saved"}
-    </Button> 
-    </div>
-      <div className='font-mont font-400 prose'>
-        <EditorContent editor={editor}/>
+      <div className="flex">
+        {editor && <TipTapMenuBar editor={editor} />}
+        <Button disabled variant={"outline"}>
+          {saveNote.isPending ? "Saving..." : "Saved"}
+        </Button>
       </div>
-    </>
-  )
-}
 
-export default TipTapEditor
+      <div className="prose prose-sm w-full mt-4">
+        <EditorContent editor={editor} />
+      </div>
+      <div className="h-4"></div>
+      <span className="text-sm font-mont font-400">
+        Tip: Press{" "}
+        <kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg">
+          Shift + A
+        </kbd>{" "}
+        for AI autocomplete
+      </span>
+    </>
+  );
+};
+
+export default TipTapEditor;
